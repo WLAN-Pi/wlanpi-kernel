8b4a2fe623caba14ae9b26c57db29aaa444ba035
Daniel Finimundi <daniel@finimundi.com>
2021-11-26 04:44:42 +0000

    Build kernel for WLAN Pi
    Initial commit to build the kernel for the WLAN Pi based on
    Raspberry Pi kernel and with our custom modifications.
    
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# wlanpi-kernel
-Sync and build the Kernel for WLAN Pi
+# Build WLAN Pi kernel
+
+Sync and build the Kernel for WLAN Pi.
+
+Currently the script downloads the kernel source from Raspberry Pi Linux Github page and does a
+checkout to specified branch.
+
+Then it applies the patches from `kernel-patches` folder to this kernel, updates the default
+defconfig with `wlanpi_defconfig`, builds the kernel and them packages it on .deb.
+
+This is a initial version, to be extended with more configuration options.
+
+Usage:
+```bash
+./build-kernel.sh <path-for-kernel-download>
+```
+
+To clean the kernel for a new compilation from scratch, just set environmental variable CLEAN_KERNEL
+to true.
+```bash
+CLEAN_KERNEL=true ./build-kernel.sh <path-for-kernel-download>
+```
+
+Kernel will only be cloned the first time. If you want to sync again (git fetch) and force the
+update to latest commit on branch, set variable FORCE_UPDATE_KERNEL to true.
+```bash
+FORCE_UPDATE_KERNEL=true ./build-kernel.sh <path-for-kernel-download>
+```
+
+The force update might fail depending on the changes made on the kernel repository. In that case,
+just use both clean and force update variables together.
+
+To be continued...
---
+++ build-kernel.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+SCRIPT_PATH="$(dirname $(realpath "$0"))"
+KERNEL_PATH="$(realpath "$1")"
+LOG_PATH="${SCRIPT_PATH}/logs"
+PATCHES_PATH="${SCRIPT_PATH}/kernel-patches"
+
+KERNEL_URL="https://github.com/raspberrypi/linux.git"
+KERNEL_BRANCH="rpi-5.15.y"
+
+mkdir -p "${LOG_PATH}"
+
+run_all()
+{
+    download_source
+
+    if [ "${CLEAN_KERNEL}" == "true" ]; then
+        clean_kernel
+    fi
+
+    apply_patches
+    generate_config
+    build_kernel
+}
+
+build_kernel()
+{
+    pushd "${KERNEL_PATH}"
+
+    log "ok" "Compile kernel"
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs | tee "${LOG_PATH}"/compilation.log 2>&1
+
+    log "ok" "Make kernel package"
+    make -j1 bindeb-pkg ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- DEBFULLNAME="Daniel Finimundi" DEBEMAIL="daniel@finimundi.com" | tee "${LOG_PATH}"/packaging.log 2>&1
+
+    log "ok" "Finished"
+
+    popd >/dev/null
+}
+
+apply_patches()
+{
+    pushd "${KERNEL_PATH}"
+
+    patches="$(find "${PATCHES_PATH}" -name "*.patch")"
+
+    log "ok" "Will apply patches:"
+    echo "${patches}"
+
+    for patch in "${patches[@]}"; do
+        if patch --no-backup-if-mismatch --silent --batch -R --dry-run -p1 -N < "${patch}"; then
+            log "warn" "Patch already applied, skipping ${patch}"
+            continue
+        fi
+
+        log "ok" "Applying patch ${patch}"
+        patch --no-backup-if-mismatch --silent --batch -p1 -N < "${patch}"
+
+        if [ $? -ne 0 ]; then
+            log "error" "Failed applying patch. Please fix it before trying again."
+            exit 2
+        fi
+    done
+
+    popd >/dev/null
+}
+
+generate_config()
+{
+    export ARCH=arm64
+    export CROSS_COMPILE=aarch64-linux-gnu-
+    export KERNEL=kernel8
+
+    cp  "${SCRIPT_PATH}/wlanpi_defconfig" "${KERNEL_PATH}/arch/arm64/configs/"
+
+    pushd "${KERNEL_PATH}"
+
+    log "ok" "Customize defconfig"
+    "${KERNEL_PATH}"/scripts/kconfig/merge_config.sh "${KERNEL_PATH}"/arch/arm64/configs/{bcm2711,wlanpi}_defconfig | tee "${LOG_PATH}"/update-config.log 2>&1
+
+    if grep -q "Actual value:" "${LOG_PATH}"/update-config.log; then
+        log "error" "Error updating defconfig. See above log to check which configs had conflicts."
+        exit 1
+    fi
+
+    popd >/dev/null
+}
+
+download_source()
+{
+    log "ok" "Downloading kernel source from ${KERNEL_URL}, branch ${KERNEL_BRANCH}"
+
+    if [ ! -d "${KERNEL_PATH}" ]; then
+        git clone --depth=1 -b "${KERNEL_BRANCH}" "${KERNEL_URL}" "${KERNEL_PATH}" | tee "${LOG_PATH}"/clone.log 2>&1
+    elif [ "${FORCE_UPDATE_KERNEL}" == "true" ]
+        pushd "${KERNEL_PATH}"
+
+        git fetch -q origin "${KERNEL_BRANCH}" | tee "${LOG_PATH}"/force-update-fetch.log 2>&1
+        git co -B "${KERNEL_BRANCH}" origin/"${KERNEL_BRANCH}" | tee "${LOG_PATH}"/force-update-checkout.log 2>&1
+
+        if [ $? -ne 0 ]; then
+            log "error" "Couldn't checkout to new kernel version. Try setting variable CLEAN_KERNEL to reset the workspace"
+            exit 3
+        fi
+
+        popd >/dev/null
+    fi
+
+    echo
+}
+
+clean_kernel()
+{
+    pushd "${KERNEL_PATH}"
+
+    log "ok" "Reseting kernel to upstream original code"
+
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- clean
+    git clean -fdx
+    git reset --hard
+    git co -B "${KERNEL_BRANCH}" origin/"${KERNEL_BRANCH}"
+
+    popd >/dev/null
+}
+
+log()
+{
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    ORANGE='\033[0;33m'
+    NC='\033[0m'
+
+    if [ "$1" == "ok" ]; then
+        echo -en "${GREEN}[ OK ]${NC} "
+    elif [ "$1" == "error" ]; then
+        echo -en "${RED}[ ERROR ]${NC} "
+    elif [ "$1" == "warn" ]; then
+        echo -en "${ORANGE}[ WARN ]${NC} "
+    fi
+    echo "$2"
+}
+
+run_all
+
