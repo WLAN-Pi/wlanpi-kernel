ba216dabc1cb830aac27eba058ff53ad2ee8dd3f
jolla <jerryolla@gmail.com>
2025-09-06 09:08:53 -0500

    Update build-kernel.sh to use 6.17 branch

--- build-kernel.sh
+++ build-kernel.sh
@@ -16,7 +16,7 @@ BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Configuration Variables
 KERNEL_REPO="https://github.com/raspberrypi/linux.git"
-KERNEL_BRANCH="rpi-6.12.y"
+KERNEL_BRANCH="rpi-6.17.y"
 KERNEL_SRC_DIR="$BASE_DIR/linux"
 OUTPUT_PATH="$BASE_DIR/output"
 CROSS_COMPILE="aarch64-linux-gnu-"
